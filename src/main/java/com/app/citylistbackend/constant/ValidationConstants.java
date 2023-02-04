2551458a59fa7cdbc0161ed493196e0bc4aa6e35
Nagesh Mishra <nagesh3.13@gmail.com>
2023-02-04 16:47:35 +0530

    Add Spring validation framework to validate the Request params to be Positive. Add Integration test to validate end to end workflow.

---
+++ src/main/java/com/app/citylistbackend/constant/ValidationConstants.java
@@ -0,0 +1,6 @@
+package com.app.citylistbackend.constant;
+
+public class ValidationConstants {
+    public static final String PAGE_MUST_BE_GREATER_THAN_0 = "Page Must Be Greater than 0";
+    public static final String PAGE_SIZE_MUST_BE_GREATER_THAN_0 = "PageSize Must Be Greater than 0";
+}
