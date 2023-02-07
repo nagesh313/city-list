ebd90b50735fe60345260fd82ad8a23f5e1d82cb
Nagesh Mishra <nagesh3.13@gmail.com>
2023-02-07 22:38:27 +0530

    Make use of spring basic security Feauture
    Make use of lazy loading in react and authenticate that js load to show username password dialog

--- src/main/java/com/app/citylistbackend/api/CityController.java
+++ src/main/java/com/app/citylistbackend/api/CityController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
@@ -49,6 +50,7 @@ public class CityController {
         return cityRepository.findByNameIgnoreCaseLike("%" + searchString + "%", Pageable.ofSize(pageSize).withPage(page));
     }
 
+    @PreAuthorize("hasRole('ALLOW_EDIT')")
     @PutMapping("/edit")
     public ResponseEntity<?> editCity(@RequestBody City city) {
         Optional<City> optionalCity = cityRepository.findById(city.getId());
---
+++ src/main/java/com/app/citylistbackend/security/BasicSpringSecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.app.citylistbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class BasicSpringSecurityConfiguration {
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("USER", "ADMIN", "ALLOW_EDIT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**/static/js/EditCityDialog**").hasRole("ALLOW_EDIT")
+                .antMatchers("/api/v1/city/edit**").hasRole("ALLOW_EDIT")
+                .antMatchers("/api/v1/**").permitAll()
+                .antMatchers("/**/static/**/main**").permitAll()
+                .antMatchers("/*").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
+
+
+}
